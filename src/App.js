081a54d75ad6b081e82f8bbbe1e9398ee5e031c1
Edward Cao <yifan.cao@uoit.net>
2022-06-15 16:22:31 -0400

    Removed Everything. Made boilerplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
+import { ethers } from "ethers";
 
-function App() {
+export default function App() {
+  const [greeting, setGreeting] = React.useState("");
+
+  const connectWallet = async () => {
+    // connect to wallet
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Please install MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      fetchGreetings();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchGreetings = async () => {
+    // fetch greetings from contract
+    let contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // contract address
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      alert("Please install MetaMask!");
+      return;
+    }
+
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(
+      contractAddress,
+      Greeter.abi,
+      provider
+    );
+    const greeting = await contract.greet();
+    setGreeting(greeting);
+  };
+
+  useEffect(() => {
+    connectWallet();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello {greeting}</h1>
     </div>
   );
 }
-
-export default App;
